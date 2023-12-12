cde5eea07937866f6efaef0eaf08ff6d6625ce2c
hardik <hardik.radicalloop@gmail.com>
2023-12-12 16:01:45 +0530

    db url change in api

--- src/module/auth/login/api.ts
+++ src/module/auth/login/api.ts
@@ -3,6 +3,6 @@ import api from "../../../shared/api";
 import { LoginFormProps } from "../type/types";
 
 export const signin = (data: LoginFormProps) =>
-  api.post("/register", data).then((res) => {
+  api.post("/login", data).then((res) => {
     setToken(res.data.access_token);
   });
